c94ba8975c4fe48a46a6a8c9c9a9875e3d3f86d2
gincuumaybe <christherber445@gmail.com>
2025-12-01 16:21:10 +0700

    perubahan url ig

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="88KA_Voaog8sYnJoLypfkDk-wVQQv-72oGXAoADr8Y0" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         integrity="sha512-pb15T5e0TtZ0bXQF5E0gXGzVjzQb+1X7J3Ht7+nXlf8fE5s5+0lG7oBvFQhMjP3G01u6+fJvC8LwR4Y+9l4H5g=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
